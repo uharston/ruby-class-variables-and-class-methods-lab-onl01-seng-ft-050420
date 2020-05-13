6d26d08e9c3683bae9cf30ca41f6ba74cc3acecf
Uriah Harston <uharston@gmail.com>
2020-05-13 18:47:06 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,87 @@
+# require 'pry'
+
+class Song
+
+  @@count = 0
+
+  @@artists = []
+
+  @@genres = []
+
+  @@artist_count = {} #{"Beyonce" => 17, "Jay-Z" => 40}
+
+  @@genre_count = {} #{"rap" => 5, "rock" => 1, "country" => 3}
+
+  #attr_accessor work only for INSTANCE variables
+  attr_accessor :artist_name, :name, :genre
+  # attr_reader :count, :artist, :genres:, :artist_count, :genres_count
+
+  def initialize(name, artist, genre)
+
+    @@count += 1
+
+    @name = name
+    @artist_name = artist
+    @genre = genre
+
+    unless @@artists.include?(artist)
+      @@artists << artist
+    end
+
+    unless @@genres.include?(genre)
+      @@genres << genre
+    end
+    #initialize @@artist_count and @@genres_count below
+
+    @@artist_count[artist] = @@artists.select { |value| value == artist}.length
+      # select_array = []
+      # select_array << value
+
+    @@genre_count[genre] = @@genres.length
+
+
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artist
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+end
+# a1 = Song.new("Both", "Drake", "Rap")
+# a2 = Song.new("Glyachester", "Drake", "Rap")
+# a3 = Song.new("Started From The Bottom", "Drake", "Rap")
+# b = Song.new("Let It Happen","Tame Impala",  "Indie")
+# c = Song.new( "What's My Age Again?","Blink 182", "Punk")
+# d = Song.new( "Niggas In Paris","Jay-Z", "Rap")
+#
+#   class Dog
+#   attr_accessor :name :breed
+#
+#   def initialize(name, breed)
+#     @name = name
+#     @breed = breed
+#   end
+#   end
+#
+#
+#
+#
+#
+#
+#
+# end
